11ef178b0ec22b05e0aa0bb966c10066f23841e7
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-04-15 02:57:01 +0300

    Fixed linter errors

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,8 +9,8 @@ class Reservation < ApplicationRecord
   private
 
   def end_date_after_start_date
-    if end_date < start_date
-      errors.add(:end_date, "must be after the start date")
-    end
+    return unless end_date < start_date
+
+    errors.add(:end_date, 'must be after the start date')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :cars, through: :reservations
 
-
   validates :name, presence: true, length: { in: 2..50 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..25 }
