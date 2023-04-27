e8e2df063ed26d244ab89c7a2f47ef40d8dc9f2b
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-04-27 21:01:09 +0800

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# admin = User.create!(name: 'Admin', username: 'admin', email: 'admin@gmail.com', password: '0000@Admin', role: 'admin')
+
+# user1 = User.create!(name: 'Abigiya', username: 'AB', email: 'AB@gmail.com', password: '12345678')
+# user2 = User.create!(name: 'user1', username: 'U1', email: 'U1@gmail.com', password: '12345678')
+
+# car1 = Car.create!(car_name: "Toyota", model: "Toyota Crown", year: 2023, color: "Black", image: "https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2023/toyotacrown/xle/4015/1l5/1.png", price: 400)
+# car2 = Car.create!(car_name: "Toyota", model: "Camry", year: 2023, color: "Gray", image: "https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2023/camry/le/2515/1j9/1.png", price: 200)
+# car3 = Car.create!(car_name: "Toyota", model: "Corolla", year: 2023, color: "Gray", image: "https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2023/corolla/le/1852/1l7/1.png", price: 200)
+# car4 = Car.create!(car_name: "Toyota", model: "GR Corolla", year: 2023, color: "Red", image: "https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2023/grcorolla/core/6281/3u5/1.png", price: 300)
+# car5 = Car.create!(car_name: "Toyota", model: "GR86", year: 2023, color: "White", image: "https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2023/gr86/gr86/6252/k1x/1.png", price: 200)
+# car6 = Car.create!(car_name: "Toyota", model: "Corolla Hybrid", year: 2023, color: "Darkers Gray", image: "https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2023/corolla/le/1852/1l7/1.png", price: 200)
+# car7 = Car.create!(car_name: "Toyota", model: "GR Supra", year: 2022, color: "Light Slate Gray", image: "https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2023/corolla/le/1852/1l7/1.png", price: 200)
+# car8 = Car.create!(car_name: "Toyota", model: "Mirai", year: 2022, color: "Light Slate Gray", image: "https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2022/mirai/xle/3002/1l5/1.png", price: 500)
+# car9 = Car.create!(car_name: "Toyota", model: "Prius", year: 2022, color: "White", image: "https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2022/prius/le/1223/1f7/1.png", price: 200)
+
+# reservation1 = Reservation.create!(start_date: '27-4-2023', end_date: '30-4-2023', user_id: 2, car_id: 4)
