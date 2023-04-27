98b1b2afa08398262f8c0ccdef6adadb09672aed
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-04-27 21:00:17 +0800

    Create routing for the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+  post '/auth/login', to: 'authentication#login'
+  post '/auth/signup/', to: 'api/v1/users#create'
+
   namespace :api do
     namespace :v1 do
-
+      resources :cars
+      resources :users, except: [:create]
+      resources :reservations
     end
   end
 end
